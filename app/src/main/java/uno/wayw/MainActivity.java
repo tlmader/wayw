3a90b1a42fc93f716ee914e7e996a292ded487fd
bcrump88 <bcrump@uno.edu>
2015-10-06 17:57:35 -0500

    Bre-Created skeleton login and register screen. Still need to implement the database

--- app/src/main/java/uno/wayw/MainActivity.java
+++ app/src/main/java/uno/wayw/MainActivity.java
@@ -1,15 +1,20 @@
 package uno.wayw;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Button registerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        register();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -48,5 +55,19 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void register(){
+        registerButton = (Button) findViewById(R.id.registerButton);
+
+        registerButton.setOnClickListener(new View.OnClickListener(){
+                    @Override
+                    public void onClick(View v) {
+                        Log.d("clicked","hit register");
+                        startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+                    }
+                }
+        );
+
     }
 }
--- app/src/main/java/uno/wayw/RegisterActivityFragment.java
+++ app/src/main/java/uno/wayw/RegisterActivityFragment.java
@@ -1,7 +1,7 @@
 package uno.wayw;
 
-import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,17 +9,14 @@ import android.view.ViewGroup;
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment {
+public class RegisterActivityFragment extends Fragment {
 
-    public MainActivityFragment() {
+    public RegisterActivityFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_main, container, false);
-
-        return view;
+        return inflater.inflate(R.layout.fragment_register, container, false);
     }
-
 }

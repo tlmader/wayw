f00d96418c03891cf7c1b004327f85d94c49f1fc
Ted Mader <tlmader@live.com>
2015-12-01 18:31:59 -0600

    Fix missing toolbar

--- app/src/main/java/uno/wayw/FeedActivity.java
+++ app/src/main/java/uno/wayw/FeedActivity.java
@@ -6,6 +6,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListView;
@@ -44,6 +45,8 @@ public class FeedActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         listView = (ListView) findViewById(R.id.list);
 
